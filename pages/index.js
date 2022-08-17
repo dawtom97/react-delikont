cfcd0de7ee04de13f2147473f67130e60badf266
Dawid <daw.tomczyk1997@gmail.com>
2022-08-17 12:34:49 +0200

    intersection observer for homepage products section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Filters } from "../src/components/Filters";
 import { Heading } from "../src/components/Heading";
 import Products from "../src/components/Products/Products";
@@ -6,20 +6,63 @@ import { magentoProducts } from "../src/graphql/magentoProducts";
 import { MainTemplate } from "../src/templates/MainTemplate";
 
 export default function Home() {
-  const [allProducts, setAllProducts] = useState();
-  const [producents, setProducents] = useState();
+  const [allProducts, setAllProducts] = useState([]);
+  const [page, setPage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const observer = useRef();
+  const lastItemRef = useRef();
 
   useEffect(() => {
-    magentoProducts().then((res) => setAllProducts(res.products.items));
+    magentoProducts(1).then((res) => {
+      setAllProducts([...res.products.items]);
+      setPage(res.products.page_info);
+    });
   }, []);
 
-  console.log(allProducts);
+  const getMoreProducts = useCallback(() => {
+    if (page.current_page >= page.total_pages || isLoading) return;
+    setIsLoading(true);
+
+    magentoProducts(page.current_page + 1).then((res) => {
+      setPage(res.products.page_info);
+      setAllProducts(prev=>[...prev, ...res.products.items]);
+      setIsLoading(false);
+    });
+  }, [page, isLoading]);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          getMoreProducts();
+        }
+      },
+      {
+        root: document,
+        threshold: 1,
+      }
+    );
+    if (lastItemRef.current) {
+      observer.current.observe(lastItemRef.current);
+    }
+
+    return () => {
+      observer.current.disconnect();
+    };
+  }, [getMoreProducts]);
+
+  
 
   return (
     <MainTemplate>
-      <Heading level='h1'>Strona główna</Heading>
-      <Filters/>
-      {!allProducts ? "Loading..." : <Products products={allProducts} />}
+      <Heading level="h1">Strona główna</Heading>
+      <Filters />
+      {!allProducts ? (
+        "Loading..."
+      ) : (
+        <Products products={allProducts} lastItem={lastItemRef} />
+      )}
+      {isLoading && <h1>LOADING</h1>}
     </MainTemplate>
   );
 }
